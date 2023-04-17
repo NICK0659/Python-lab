ec7bafcb24b7afb003f0a3246d15f4db1179c3f2
Kratika2601 <105591254+Kratika2601@users.noreply.github.com>
2023-04-17 10:28:34 +0530

    added

---
+++ class programs.py
@@ -0,0 +1,50 @@
+#st="welcome to python"
+'''for i in st:
+    print(i,end=' ')'''
+
+'''i=0
+while i<len(st):
+    print(st[i],end=' ')
+    i=i+1'''
+
+
+'''
+!!!string modules!!!
+
+st.ascii.letters
+st.ascii.lowercase
+st.ascii.uppercase
+st.digits
+st.hexadigits
+st.lowercase
+st.octigits
+st.punctuation
+st.printable
+st.uppercase
+st.whitespace
+'''
+
+'''print("uppercase- ",st.upper())
+
+tup=(1,2,3,4)
+print(tup)
+print(tup[1])'''
+
+
+'''dic={1:"abc",2:"def",4:"xyz"}
+print(dic)
+print(dic[1])
+print(dic.get(2))
+print(dic.get(3,"not found"))
+keys=['pqr','str','you']
+values=['python','java','c++']
+data=dict(zip(keys,values))
+print(data)'''
+
+prog={"js":"atom","cs":"v","python":["pycharm","sublime"],"java":{"jse":"netbeans","jee":"eclipse"}}
+print(prog)
+
+
+
+
+
